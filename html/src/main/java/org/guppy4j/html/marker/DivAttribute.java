54faad0b7e0f366a4eb1fe61e99fa49511354f2b
Oliver Doepner <oliver@doepner.net>
2016-01-09 23:24:05 -0400

    Forgot the marker interfaces ...

---
+++ html/src/main/java/org/guppy4j/html/marker/DivAttribute.java
@@ -0,0 +1,7 @@
+package org.guppy4j.html.marker;
+
+import org.guppy4j.html.attribute.Attribute;
+
+@FunctionalInterface
+public interface DivAttribute<M> extends Attribute<M> {
+}
---
+++ html/src/main/java/org/guppy4j/html/marker/LegendAttribute.java
@@ -0,0 +1,7 @@
+package org.guppy4j.html.marker;
+
+import org.guppy4j.html.attribute.Attribute;
+
+@FunctionalInterface
+public interface LegendAttribute<M> extends Attribute<M> {
+}
